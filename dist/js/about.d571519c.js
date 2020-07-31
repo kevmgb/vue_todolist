42df68a3c2ea50d400d6f92f74d2a781bdf6e647
Kevin K <kithinjimkevin@gmail.com>
2020-07-31 16:28:41 +0300

    remove dist for gitignore

---
+++ dist/js/about.d571519c.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(t,e,n){"use strict";n.r(e);var s=function(){var t=this,e=t.$createElement;t._self._c;return t._m(0)},u=[function(){var t=this,e=t.$createElement,n=t._self._c||e;return n("div",{staticClass:"about"},[n("h1",[t._v("About")]),n("p",[t._v("This is the TodoList App v1.0.0.")])])}],a=n("2877"),i={},o=Object(a["a"])(i,s,u,!1,null,null,null);e["default"]=o.exports}}]);
+//# sourceMappingURL=about.d571519c.js.map
